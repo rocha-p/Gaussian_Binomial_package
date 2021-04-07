53cccdfb521b57d6c8f8ed3c4b60bc85b2e2a0fd
Pedro Rocha <rochapedroignacio@gmail.com>
2021-04-07 13:59:18 -0300

    docs: fix documentation

--- dist_binom_gauss/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='dist_binom_gauss',
-      version='1.0',
-      description='Gaussian and binomial distributions',
-      packages=['dist_binom_gauss'],
-      author='Pedro Ignacio Rocha',
-      author_email = 'rochapedroignacio@gmail.com',
-      zip_safe=False)
